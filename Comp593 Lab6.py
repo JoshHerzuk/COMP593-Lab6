a63ade50fd138015a814b276b761f6366c71c28c
JoshHerzuk <98534840+JoshHerzuk@users.noreply.github.com>
2022-03-31 10:06:55 -0400

    final script with comments added

--- Comp593 Lab6.py
+++ Comp593 Lab6.py
@@ -1,3 +1,18 @@
+#
+#
+# Description:
+#   This scripts uses the poke API to gather the name and abilites of a selected pokemon
+#
+# Usage:
+#   python Comp593 lab6.py poke_name
+#
+# Parameters:
+#   poke_name = the pokemon you want the information for
+#
+#
+##########################################################################################################################
+
+
 import requests
 from sys import argv
 
@@ -5,12 +20,13 @@ from sys import argv
 def main():
     poke_name = argv[1]   
     
+    #gathers the information of the selected pokemon in to a json library
     poke_info = get_poke_info(poke_name)
 
     if poke_info:
-
+        #creates the string from the library that contains the pokemon's name and abilitied
         pb_strings = poke_name_and_abilities(poke_info)
-
+        #pastes the pokemon's information to pastebin
         pb_url = post_to_pastebin(pb_strings[0], pb_strings[1])
 
         print(pb_url)
@@ -18,7 +34,7 @@ def main():
  
 
 def get_poke_info(poke_name): 
-
+    #queries the poke API for information related to the pokemon specified in the command line param
     print("Retrieving Pokemon information...", end='')
     URL = 'https://pokeapi.co/api/v2/pokemon/'
     response = requests.get(URL + poke_name)
@@ -32,7 +48,7 @@ def get_poke_info(poke_name):
         return
           
 def poke_name_and_abilities(poke_dict):
-
+    #builds the string that will be posted to pastebin from the pokemon's library
     name = poke_dict['name'] + "'s Abilities."
     abilities = " "
     for i in poke_dict['abilities']:
@@ -41,7 +57,7 @@ def poke_name_and_abilities(poke_dict):
     return(name, abilities)
 
 def post_to_pastebin(name, abilities):
-
+    #posts the pokemon's information to pastebin
     print("Posting to Pastebin...", end='')
 
     params = {
